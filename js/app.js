dff054980f648373c1984c93017dbe17194690c7
Jamie Alden <jamielynn_21@Macintosh.local>
2016-08-24 23:27:23 -0600

    New Game and Guess Counter

--- js/app.js
+++ js/app.js
@@ -12,6 +12,21 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-});
+	/*---Start a new game---*/
+	$("a.new").click(function() {
+		$("#count").text("0");
+		$("#guessList").text("");
+	});
 
+	/*---Number of guesses---*/
+
+	  var count = 0;
+      var display = document.getElementById("count");
+
+      $("#guessButton").click(function(){
+        count++;
+        display.innerHTML = count;
+    });
+
+});
 
